40ad5fb54f7e7ff395a4b8cd42140d27f6c23c38
Dominic Schwarz <mail@dominicschwarz.de>
2020-10-07 23:58:22 +0200

    chore: static again

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
 	// Target (https://go.nuxtjs.dev/config-target)
-	target: 'server',
+	target: 'static',
+	// target: 'server',
 
 	serverMiddleware: ['~/server-middleware/log.js'],
 
