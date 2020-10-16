8c2e25c49b9f3b048f732942e08ee32ab7ce99af
Dominic Schwarz <mail@dominicschwarz.de>
2020-10-17 00:21:46 +0200

    feat: add band logo

--- components/BandDetail.vue
+++ components/BandDetail.vue
@@ -1,6 +1,17 @@
 <template>
 	<div v-editable="band" class="prose my-24 mx-auto">
 		<h1>{{ band.name }}</h1>
+		<v-img
+			v-if="band.logo.filename"
+			:src="band.logo.filename"
+			:alt="'Logo'"
+			eager
+			class="py-3"
+			max-width="300"
+			:transition="false"
+			position="top center"
+			contain
+		></v-img>
 		<p>{{ band.description }}</p>
 		<!-- <rich-text-renderer :document="band.body" /> -->
 	</div>
