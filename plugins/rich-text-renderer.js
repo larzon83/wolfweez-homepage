bdec61ca7c4524fe7112f9ff9d589476b48342dd
Dominic Schwarz <mail@dominicschwarz.de>
2020-10-27 01:17:33 +0100

    fix: add a new sb link resolver

--- plugins/rich-text-renderer.js
+++ plugins/rich-text-renderer.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
+import { Mark } from '@marvr/storyblok-rich-text-types'
 import VueRichTextRenderer from '@marvr/storyblok-rich-text-vue-renderer'
+import StoryblokLinkResolver from '../utils/StoryblokLinkResolver.vue'
 
-Vue.use(VueRichTextRenderer)
+Vue.use(VueRichTextRenderer, {
+	resolvers: {
+		marks: {
+			[Mark.LINK]: StoryblokLinkResolver
+		}
+	}
+})
