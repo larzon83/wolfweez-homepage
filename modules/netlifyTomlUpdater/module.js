26281ce673a45b2d6d7156c5fb81dbc056b715d5
Dominic Schwarz <mail@dominicschwarz.de>
2020-10-30 00:31:22 +0100

    chore: try netlify-files again

--- modules/netlifyTomlUpdater/module.js
+++ modules/netlifyTomlUpdater/module.js
@@ -1,66 +1,67 @@
-import axios from 'axios'
-import fse from 'fs-extra'
-const { resolve } = require('path')
+// import axios from 'axios'
+// import fse from 'fs-extra'
+// const { resolve } = require('path')
 
 export default async function (moduleOptions) {
-	const { data } = await axios.get(
-		`https://api.storyblok.com/v1/cdn/stories?is_startpage=true&starts_with=historie/&sort_by=slug:desc&token=${process.env.STORYBLOK_TOKEN}`
-	)
+	// const { data } = await axios.get(
+	// 	`https://api.storyblok.com/v1/cdn/stories?is_startpage=true&starts_with=historie/&sort_by=slug:desc&token=${process.env.STORYBLOK_TOKEN}`
+	// )
 
-	const srcFile = resolve(this.options.srcDir, 'netlify.toml')
+	// const srcFile = resolve(this.options.srcDir, 'netlify.toml')
 
-	const contentRaw = fse.readFileSync(srcFile, 'UTF-8')
+	// const contentRaw = fse.readFileSync(srcFile, 'UTF-8')
 
-	const regEx = /(to = "\/historie\/)(\w+)(\/")/i
+	// const regEx = /(to = "\/historie\/)(\w+)(\/")/i
 
-	if (!contentRaw.match(regEx)) {
-		throw new Error('netlifyTomlUpdater: regular Expression failed')
-	}
-
-	const contentNew = contentRaw.replace(regEx, `$1${data.stories[0].slug}$3`)
-
-	fse.writeFileSync(srcFile, contentNew, 'UTF-8')
-
-	// const netlifyToml = {
-	// 	redirects: [
-	// 		{
-	// 			from: '/line-up/',
-	// 			to: '/line-up/bands/',
-	// 			force: true
-	// 		},
-	// 		{
-	// 			from: '/historie/',
-	// 			to: '/historie/2019/',
-	// 			force: true
-	// 		}
-	// 	],
-	// 	plugins: [
-	// 		{
-	// 			package: '@netlify/plugin-sitemap',
-	// 			inputs: {
-	// 				changeFreq: 'daily',
-	// 				priority: 0.5,
-	// 				// Append missing trailing slash to pretty URL
-	// 				trailingSlash: true,
-	// 				exclude: [
-	// 					// './dist/200.html',
-	// 					// '**/historie/index.html',
-	// 					// '**/line-up/index.html',
-	// 					// '**/**/200.html'
-	// 					'./historie/index.html',
-	// 					'./line-up/index.html',
-	// 					'./**/200.html'
-	// 				]
-	// 			}
-	// 		}
-	// 	]
+	// if (!contentRaw.match(regEx)) {
+	// 	throw new Error('netlifyTomlUpdater: regular Expression failed')
 	// }
 
-	// this.requireModule([
-	// 	'@nuxtjs/netlify-files',
-	// 	{
-	// 		copyExistingFiles: false,
-	// 		netlifyToml
-	// 	}
-	// ])
+	// const contentNew = contentRaw.replace(regEx, `$1${data.stories[0].slug}$3`)
+	// console.log('contentNew:', contentNew)
+
+	// fse.writeFileSync(srcFile, contentNew, 'UTF-8')
+
+	const netlifyToml = {
+		redirects: [
+			{
+				from: '/line-up/',
+				to: '/line-up/bands/',
+				force: true
+			},
+			{
+				from: '/historie/',
+				to: '/historie/2019/',
+				force: true
+			}
+		],
+		plugins: [
+			{
+				package: '@netlify/plugin-sitemap',
+				inputs: {
+					changeFreq: 'daily',
+					priority: 0.5,
+					// Append missing trailing slash to pretty URL
+					trailingSlash: true,
+					exclude: [
+						// './dist/200.html',
+						// '**/historie/index.html',
+						// '**/line-up/index.html',
+						// '**/**/200.html'
+						'./historie/index.html',
+						'./line-up/index.html',
+						'./**/200.html'
+					]
+				}
+			}
+		]
+	}
+
+	await this.requireModule([
+		'@nuxtjs/netlify-files',
+		{
+			copyExistingFiles: false,
+			netlifyToml
+		}
+	])
 }
