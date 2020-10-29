5fcad765a972e3affaef5d237d05c27636818422
Dominic Schwarz <mail@dominicschwarz.de>
2020-10-29 14:42:45 +0100

    chore: test smtg

--- modules/netlifyTomlGenerator/module.js
+++ modules/netlifyTomlGenerator/module.js
@@ -1,13 +1,6 @@
 import axios from 'axios'
-
-// export default function () {
-// 	this.nuxt.hook('build:before', async context => {
-// 		const { data } = await axios.get(
-// 			`https://api.storyblok.com/v1/cdn/stories?is_startpage=true&starts_with=historie/&sort_by=slug:desc&token=${process.env.STORYBLOK_TOKEN}`
-// 		)
-// 		console.log('axios:', data)
-// 	})
-// }
+import fse from 'fs-extra'
+const { resolve } = require('path')
 
 export default async function (moduleOptions) {
 	const { data } = await axios.get(
@@ -15,47 +8,53 @@ export default async function (moduleOptions) {
 	)
 	console.log('axios:', data.stories[0].slug)
 
-	const netlifyToml = {
-		redirects: [
-			{
-				from: '/line-up/',
-				to: '/line-up/bands/',
-				force: true
-			},
-			{
-				from: '/historie/',
-				// to: '/historie/2019/',
-				to: '/historie/2018/', // FIXME:
-				force: true
-			}
-		],
-		plugins: [
-			{
-				package: '@netlify/plugin-sitemap',
-				inputs: {
-					changeFreq: 'daily',
-					priority: 0.5,
-					// Append missing trailing slash to pretty URL
-					trailingSlash: true,
-					exclude: [
-						// './dist/200.html',
-						// '**/historie/index.html',
-						// '**/line-up/index.html',
-						// '**/**/200.html'
-						'./historie/index.html',
-						'./line-up/index.html',
-						'./**/200.html'
-					]
-				}
-			}
-		]
-	}
+	const srcFile = resolve(this.options.srcDir, 'netlify.toml')
 
-	this.requireModule([
-		'@nuxtjs/netlify-files',
-		{
-			copyExistingFiles: false,
-			netlifyToml
-		}
-	])
+	// const contentRaw = fse.readFileSync('../../netlify.toml', 'UTF-8')
+	const contentRaw = fse.readFileSync(srcFile, 'UTF-8')
+	console.log('contentRaw:', contentRaw)
+
+	// const netlifyToml = {
+	// 	redirects: [
+	// 		{
+	// 			from: '/line-up/',
+	// 			to: '/line-up/bands/',
+	// 			force: true
+	// 		},
+	// 		{
+	// 			from: '/historie/',
+	// 			// to: '/historie/2019/',
+	// 			to: '/historie/2018/', // FIXME:
+	// 			force: true
+	// 		}
+	// 	],
+	// 	plugins: [
+	// 		{
+	// 			package: '@netlify/plugin-sitemap',
+	// 			inputs: {
+	// 				changeFreq: 'daily',
+	// 				priority: 0.5,
+	// 				// Append missing trailing slash to pretty URL
+	// 				trailingSlash: true,
+	// 				exclude: [
+	// 					// './dist/200.html',
+	// 					// '**/historie/index.html',
+	// 					// '**/line-up/index.html',
+	// 					// '**/**/200.html'
+	// 					'./historie/index.html',
+	// 					'./line-up/index.html',
+	// 					'./**/200.html'
+	// 				]
+	// 			}
+	// 		}
+	// 	]
+	// }
+
+	// this.requireModule([
+	// 	'@nuxtjs/netlify-files',
+	// 	{
+	// 		copyExistingFiles: false,
+	// 		netlifyToml
+	// 	}
+	// ])
 }
