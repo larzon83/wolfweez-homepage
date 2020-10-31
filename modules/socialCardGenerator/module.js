4754e3dfe44f116c56d82d7f57fb96311a0ff8b7
Dominic Schwarz <mail@dominicschwarz.de>
2020-11-01 00:47:18 +0100

    refactor(seo): generate nice og:images

--- modules/socialCardGenerator/module.js
+++ modules/socialCardGenerator/module.js
@@ -1,10 +1,11 @@
 import StoryblokClient from 'storyblok-js-client'
 import { ogImagesDir, removeSlashesFromStartAndEnd } from './../../utils/seo'
 
+const { resolve } = require('path')
 const fs = require('fs-extra')
 const nodeHtmlToImage = require('node-html-to-image')
 
-let isTest = true
+let isTest = false
 if (process.env.NODE_ENV === 'production') isTest = false
 
 const hookName = isTest ? 'build:before' : 'generate:done'
@@ -30,22 +31,35 @@ let allRoutes = [
 	'/historie/2018/'
 ]
 
+const imageSrc = resolve(
+	__dirname,
+	'wolfweez-open-air-festival_og-background.png'
+)
+const image = fs.readFileSync(imageSrc)
+// eslint-disable-next-line new-cap
+const base64Image = new Buffer.from(image).toString('base64')
+const bgImage = 'data:image/jpeg;base64,' + base64Image
+
+const htmlTemplateSrc = resolve(__dirname, 'html-template.html')
+const htmlTemplate = fs.readFileSync(htmlTemplateSrc, 'UTF-8')
+
 const createImages = async content => {
 	await nodeHtmlToImage({
 		content,
 		puppeteerArgs: { args: ['--no-sandbox'] },
-		html: `<html>
-			<head>
-				<style>
-					body {
-						width: 1200px;
-						height: 630px;
-					}
-				</style>
-			</head>
-			<body><h1>{{headline}}</h1><h5>{{subline}}</h5></body>
-		</html>
-		`
+		html: htmlTemplate
+		// html: `<html>
+		// 	<head>
+		// 		<style>
+		// 			body {
+		// 				width: 1200px;
+		// 				height: 630px;
+		// 			}
+		// 		</style>
+		// 	</head>
+		// 	<body><h1>{{headline}}</h1><h5>{{subline}}</h5></body>
+		// </html>
+		// `
 	}).then(() => console.log('successfully created social card images!'))
 }
 
@@ -93,6 +107,7 @@ export default function () {
 		const content = []
 
 		for (const routeRaw of finalRoutes) {
+			let bodyClass = ''
 			let subline = ''
 			let headline = ''
 			const route = removeSlashesFromStartAndEnd(routeRaw)
@@ -104,6 +119,7 @@ export default function () {
 				if (splitted[0] === 'historie') subline = splitted[0].toUpperCase()
 				if (splitted[0] === 'news') {
 					subline = splitted[0].toUpperCase()
+					bodyClass = 'is-news'
 					const headlineFromStoryblok = newsData.filter(news => {
 						return news.slug === splitted[1]
 					})
@@ -116,6 +132,8 @@ export default function () {
 			content.push({
 				headline,
 				subline,
+				bgImage,
+				bodyClass,
 				output: `${dir}/${outputFilePath}.png`
 			})
 		}
