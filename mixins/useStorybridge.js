d9ff62c4a45a8cc5a3f9aff152ea893ff451b6d2
Dominic Schwarz <brunooo@gmx.de>
2021-06-04 16:37:11 +0200

    chore: update storyblok-nuxt

--- mixins/useStorybridge.js
+++ mixins/useStorybridge.js
@@ -8,51 +8,69 @@ export default {
 		// 	}
 		// )
 
-		// Use the input event for instant update of content
-		this.$storybridge.on('input', async event => {
-			if (event.story.content.component === 'timetable') {
-				const { data: bands } = await this.$storyapi.get('cdn/stories', {
-					filter_query: {
-						component: {
-							in: 'band'
+		// load the bridge only inside of Storyblok
+		if (window.location.search.includes('_storyblok')) {
+			this.$storybridge(
+				() => {
+					const { StoryblokBridge } = window
+
+					const storyblokInstance = new StoryblokBridge()
+
+					storyblokInstance.on(
+						['input', 'published', 'change'],
+						async event => {
+							if (event.action === 'input') {
+								if (event.story.content.component === 'timetable') {
+									const { data: bands } = await this.$storyapi.get(
+										'cdn/stories',
+										{
+											filter_query: {
+												component: {
+													in: 'band'
+												}
+											},
+											version:
+												process.env.NUXT_ENV_STORYBLOK_PREVIEW === 'true'
+													? 'draft'
+													: 'published'
+										}
+									)
+
+									event.story.content.entry.forEach((day, _indexDay) => {
+										day.entry.forEach((band, _indexBand) => {
+											const currentBand =
+												this.story.content.entry[_indexDay].entry[_indexBand]
+													.band
+
+											// another band was selected in dropdown
+											if (band.band.id !== currentBand.id) {
+												const newBand = bands.stories.filter(
+													item => item.uuid === band.band.id
+												)
+												band.band.story = newBand[0]
+											} else {
+												band.band = currentBand
+											}
+										})
+									})
+								}
+
+								if (event.story.id === this.story.id) {
+									this.story.content = event.story.content
+								}
+							} else {
+								this.$nuxt.$router.go({
+									path: this.$nuxt.$router.currentRoute,
+									force: true
+								})
+							}
 						}
-					},
-					version:
-						process.env.NUXT_ENV_STORYBLOK_PREVIEW === 'true'
-							? 'draft'
-							: 'published'
-				})
-
-				event.story.content.entry.forEach((day, _indexDay) => {
-					day.entry.forEach((band, _indexBand) => {
-						const currentBand =
-							this.story.content.entry[_indexDay].entry[_indexBand].band
-
-						// another band was selected in dropdown
-						if (band.band.id !== currentBand.id) {
-							const newBand = bands.stories.filter(
-								item => item.uuid === band.band.id
-							)
-							band.band.story = newBand[0]
-						} else {
-							band.band = currentBand
-						}
-					})
-				})
-			}
-
-			if (event.story.id === this.story.id) {
-				this.story.content = event.story.content
-			}
-		})
-
-		// Use the bridge to listen the events
-		this.$storybridge.on(['published', 'change'], event => {
-			// window.location.reload()
-			this.$nuxt.$router.go({
-				path: this.$nuxt.$router.currentRoute,
-				force: true
-			})
-		})
+					)
+				},
+				error => {
+					console.error(error)
+				}
+			)
+		}
 	}
 }
