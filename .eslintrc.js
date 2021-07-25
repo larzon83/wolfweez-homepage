d5a7be349ac7e56097d13ba7d5d9b46bf721b0bc
Dominic Schwarz <brunooo@gmx.de>
2021-07-26 01:41:36 +0200

    chore: update configs to align with a new nuxt project

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,15 @@ module.exports = {
 		node: true
 	},
 	parserOptions: {
-		parser: 'babel-eslint'
+		parser: '@babel/eslint-parser',
+		requireConfigFile: false
 	},
 	extends: [
 		'@nuxtjs',
-		'prettier',
-		'plugin:prettier/recommended',
-		'plugin:nuxt/recommended'
+		'plugin:nuxt/recommended',
+		'prettier'
 	],
-	plugins: ['prettier'],
+	plugins: [],
 	// add your custom rules here
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
