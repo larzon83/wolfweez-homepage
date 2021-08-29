c5d5b0df30533d5d59f7d92c4acfbd7a028b8b91
Dominic Schwarz <brunooo@gmx.de>
2021-08-29 20:44:11 +0200

    feat(timetable): start design

--- pages/line-up/timetable.vue
+++ pages/line-up/timetable.vue
@@ -1,41 +1,50 @@
 <template>
-	<section>
-		<v-row v-if="story.content.entry" class="timetable my-0 px-4">
-			<v-col
-				v-for="day in story.content.entry"
-				:key="day._uid"
-				v-editable="day"
-				cols="6"
-				class="day"
-			>
-				<h2>
-					{{ day.day_display_name }}
-				</h2>
-				<div>
-					{{ $_niceDate(day.day) }}
-				</div>
-				<br />
-				<br />
-				<br />
-
-				<div
-					v-for="slot in day.entry"
-					:key="slot._uid"
-					v-editable="slot"
-					class="slot"
-				>
-					TIME: {{ $_playTime(slot.time_start, slot.time_end) }}<br /><br />
-					BAND:
-					<nuxt-link :to="$_slashify(slot.band.story.full_slug)">{{
-						slot.band.story.content.name
-					}}</nuxt-link>
-					<br />
-					<br />
-					<br />
-				</div>
-			</v-col>
-		</v-row>
-	</section>
+	<v-row v-if="story.content.entry" class="timetable" tag="section">
+		<v-col
+			v-for="day in story.content.entry"
+			:key="day._uid"
+			v-editable="day"
+			cols="12"
+			lg="6"
+		>
+			<v-card color="darkish" flat height="100%" class="timetable-card">
+				<v-card-title class="timetable-header justify-center text-center pb-3">
+					<div class="flex-column justify-center">
+						<h2>{{ day.day_display_name }}</h2>
+						<div
+							class="timetable-header__date text-subtitle-1 font-weight-medium"
+						>
+							{{ $_niceDate(day.day) }}
+						</div>
+					</div>
+				</v-card-title>
+				<v-card-text class="pt-5">
+					<div
+						v-for="(slot, index) in day.entry"
+						:key="slot._uid"
+						v-editable="slot"
+						class="slot"
+					>
+						<nuxt-link :to="$_slashify(slot.band.story.full_slug)">{{
+							slot.band.story.content.name
+						}}</nuxt-link
+						><br />
+						TIME: {{ $_playTime(slot.time_start, slot.time_end) }}
+						<v-divider v-if="index !== day.entry.length - 1" class="my-8" />
+					</div>
+				</v-card-text>
+				<!-- <v-img
+					v-if="band.content.image && band.content.image.filename"
+					:alt="band.content.name"
+					:src="$_transformImage(band.content.image.filename, '600x0')"
+					:lazy-src="$_transformImage(band.content.image.filename, '300x0')"
+					:aspect-ratio="$config.aspectRatios.BAND"
+					eager
+					:position="$_shiftImagePositionY(band.content.image_offset)"
+				></v-img> -->
+			</v-card>
+		</v-col>
+	</v-row>
 </template>
 
 <script>
@@ -88,17 +97,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// .timetable {
-// 	display: flex;
-// 	flex-shrink: 0;
-// 	flex-grow: 1;
-// 	align-items: flex-start;
-// 	justify-content: center;
-// }
-.day {
-	border: 1px solid getcolor('prime');
-}
-.slot {
-	border-top: 1px solid getcolor('prime');
+.timetable {
+	.timetable-card {
+		border: 3px solid getcolor('prime') !important;
+
+		.timetable-header {
+			background-color: getcolor('prime');
+			color: getcolor('dark');
+			text-transform: uppercase;
+			border-radius: 0 !important;
+
+			.timetable-header__date {
+				color: getcolor('dark', 0.65);
+			}
+		}
+	}
 }
 </style>
