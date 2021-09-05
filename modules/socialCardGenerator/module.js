f67610bfcec1c691247f079bf6fa061dce4d8854
Dominic Schwarz <brunooo@gmx.de>
2021-09-06 00:37:55 +0200

    refactor(module-socialCardGenerator): update logic

--- modules/socialCardGenerator/module.js
+++ modules/socialCardGenerator/module.js
@@ -1,43 +1,59 @@
 import StoryblokClient from 'storyblok-js-client'
-import { ogImagesDir, removeSlashesFromStartAndEnd } from './../../utils/seo'
+import { slashify } from './../../utils'
+import {
+	dashifyPath,
+	ogImagesDir,
+	removeSlashesFromStartAndEnd
+} from './../../utils/seo'
 
 const { resolve } = require('path')
 const fs = require('fs-extra')
 const nodeHtmlToImage = require('node-html-to-image')
 
-let isTest = false
+let isTest = true
 if (process.env.NODE_ENV === 'production') isTest = false
 
 const hookName = isTest ? 'build:before' : 'generate:done'
 const dirStatic = `./static/${ogImagesDir}`
 let dir = dirStatic
-let allRoutes = [
-	'/infos/',
-	'/line-up/',
-	'/kontakt/',
-	'/rechtliches/',
-	'/newsletter/',
-	'/historie/',
-	'/news/',
+let nuxtGeneratedRoutes = [
+	'/',
 	'/fotogalerie/',
+	'/historie/',
+	'/infos/',
+	'/kontakt/',
+	'/line-up/',
+	'/news/',
+	'/rechtliches/',
+	'/sponsoren/',
+	'/tickets/',
 	'/line-up/bands/',
 	'/line-up/timetable/',
-	'/rechtliches/agb/',
-	'/rechtliches/impressum/',
-	'/rechtliches/datenschutz/',
-	'/news/third-one/',
 	'/infos/allgemein/',
+	'/infos/testing/',
 	'/infos/anreise/',
+	'/infos/faq/',
+	'/historie/2019/',
+	'/historie/2018/',
+	'/rechtliches/agb/',
+	'/rechtliches/datenschutz/',
+	'/rechtliches/impressum/',
+	'/news/wolfweez-openair-festival-auf-2022-verschoben/',
+	'/news/dolle-news/',
+	'/news/early-wolf-aktion-2021-endet/',
+	'/news/third-one/',
 	'/news/second-news/',
 	'/news/first-news-ever/',
+	'/news/ab-sofort-early-wolf-tickets-2021-verfuegbar/',
+	'/news/wolfweez-festival-paket-2021-erhaeltlich/',
 	'/line-up/bands/grave-digger/',
-	'/historie/2019/',
-	'/line-up/bands/super-duper/',
-	'/line-up/bands/brainstorm/',
-	'/line-up/bands/foobar/',
 	'/line-up/bands/voltbeat/',
-	'/historie/2018/',
-	'/line-up/bands/the-prophecy-23/'
+	'/line-up/bands/brainstorm/',
+	'/line-up/bands/the-prophecy-23/',
+	'/line-up/bands/motoerbloeck/',
+	'/line-up/bands/vulgar-display-of-cover/',
+	'/line-up/bands/venues/',
+	'/line-up/bands/endless-decay/'
 ]
 
 const imageSrc = resolve(
@@ -73,34 +89,45 @@ export default function () {
 		this.nuxt.hook(hookName, async context => {
 			if (!isTest) {
 				dir = `${context.options.generate.dir}/${ogImagesDir}`
-				allRoutes = await Array.from(context.generatedRoutes)
+				nuxtGeneratedRoutes = await Array.from(context.generatedRoutes)
 			}
 
 			const client = new StoryblokClient({
 				accessToken: process.env.STORYBLOK_TOKEN
 			})
 
-			const { data } = await client.get('cdn/stories', {
-				starts_with: 'news/',
-				is_startpage: 0,
-				sort_by: 'created_at:desc'
+			const allStories = await client.getAll('cdn/stories', {
+				version:
+					process.env.NUXT_ENV_STORYBLOK_PREVIEW === 'true'
+						? 'draft'
+						: 'published'
 			})
 
-			const newsData = data.stories.map(item => {
-				return {
-					slug: item.slug,
-					headline: item.content.headline
-				}
+			const storiesWithSocialImage = allStories.filter(
+				item =>
+					item.path !== '/' &&
+					(item.content?.image_social?.filename ||
+						(item.content.component === 'band' &&
+							item.content?.image?.filename))
+			)
+
+			const socialRoutes = storiesWithSocialImage.map(item => {
+				const path = item.path || item.full_slug
+				return slashify(path)
 			})
 
 			if (!fs.existsSync(dir)) {
 				fs.mkdirSync(dir)
 			}
 
-			const finalRoutes = allRoutes.filter(route => {
-				const splitted = route.split('/')
-				if (splitted[2] === 'bands' && splitted[3]) return false
+			const finalRoutes = nuxtGeneratedRoutes.filter(route => {
 				if (route === '/') return false
+
+				const hasSocialImage = socialRoutes.find(
+					socialRoute => socialRoute === route
+				)
+				if (hasSocialImage) return false
+
 				return route
 			})
 
@@ -111,19 +138,22 @@ export default function () {
 				let subline = ''
 				let headline = ''
 				const route = removeSlashesFromStartAndEnd(routeRaw)
-				const outputFilePath = route.replace('/', '-')
+				const outputFilePath = dashifyPath(route)
 				const splitted = route.split('/')
+
+				const story = allStories.find(item => item.full_slug === route)
 
 				if (splitted.length > 1) {
 					headline = splitted[1].toUpperCase()
 					if (splitted[0] === 'historie') subline = splitted[0].toUpperCase()
-					if (splitted[0] === 'news') {
-						subline = splitted[0].toUpperCase()
+					if (splitted[0] === 'news' && splitted[1]) {
 						bodyClass = 'is-news'
-						const headlineFromStoryblok = newsData.find(news => {
-							return news.slug === splitted[1]
-						})
-						headline = headlineFromStoryblok.headline
+						headline = story.content.headline || splitted[1]
+						subline = splitted[0].toUpperCase()
+					}
+					if (splitted[1] === 'bands' && splitted[2]) {
+						headline =
+							story.content.name.toUpperCase() || splitted[2].toUpperCase()
 					}
 				} else {
 					headline = route.toUpperCase()
