54aa56e559058a89a22aaf41439f5ec110ad25bb
Dominic Schwarz <brunooo@gmx.de>
2021-09-18 00:02:50 +0200

    refactor(_historie): add preload images

--- pages/historie/_historie.vue
+++ pages/historie/_historie.vue
@@ -94,6 +94,13 @@ export default {
 			this.$route.path
 		)
 
+		const linkEntries = []
+		const preloadImage = this.$_getPreloadImageHeadEntry(
+			this.story.content.flyer?.filename,
+			this.$config.presetNames.HISTORY_FLYER
+		)
+		if (preloadImage) linkEntries.push(preloadImage)
+
 		return {
 			title,
 			meta: createSEOMeta({
@@ -103,7 +110,8 @@ export default {
 				imageHeight,
 				title,
 				url: this.$route.path
-			})
+			}),
+			link: [...linkEntries]
 		}
 	},
 
