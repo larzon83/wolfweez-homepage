2f74ffc837559b8c85bafd7ac4895c1c86c74348
Dominic Schwarz <brunooo@gmx.de>
2021-09-27 14:07:11 +0200

    refactor(functions): pretty console

--- functions/stripe-checkout.js
+++ functions/stripe-checkout.js
@@ -10,7 +10,6 @@ const SITE_URL = process.env.URL || 'http://localhost:3000'
 
 const handler = async event => {
 	const { countryNames } = require('./../utils/stripe-helpers.js')
-	let session
 
 	if (event.httpMethod !== 'POST') {
 		return {
@@ -22,19 +21,12 @@ const handler = async event => {
 		}
 	}
 
-	const eventBody = JSON.parse(event.body)
-	console.info('eventBody:', eventBody)
-
-	const countryKeys = Object.keys(countryNames)
+	let session
 
 	const items = JSON.parse(event.body).map(item => ({
 		...item,
 		adjustable_quantity: { enabled: true }
 	}))
-
-	// const priceFoo = await stripe.prices.retrieve(price)
-	// const unitAmount = priceFoo.unit_amount
-	// const currency = priceFoo.currency
 
 	try {
 		session = await stripe.checkout.sessions.create({
@@ -53,18 +45,17 @@ const handler = async event => {
 			billing_address_collection: 'required',
 			shipping_rates: ['shr_1Je60JBfAFuG6uOs6j4oGfs6'],
 			shipping_address_collection: {
-				allowed_countries: countryKeys
+				allowed_countries: Object.keys(countryNames)
 			},
 			line_items: items
 			// automatic_tax: { enabled: false, status: null }, // TODO:
-			// shipping_rates: ['shr_123456789'], // TODO: https://stripe.com/docs/payments/checkout/shipping
 		})
 	} catch (error) {
-		console.error('checkout:', error)
+		console.error('❌  checkout:', error)
 		return { statusCode: error.statusCode, body: JSON.stringify(error) }
 	}
 
-	console.info('session:', session)
+	console.info('ℹ️  session:', session)
 
 	return {
 		statusCode: 200,
