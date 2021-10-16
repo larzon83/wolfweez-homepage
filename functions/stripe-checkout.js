b3d059e61040ed161e9226587838af6e9ec47d03
Dominic Schwarz <brunooo@gmx.de>
2021-10-16 23:13:38 +0200

    refactor(stripe): remove 'ideal' from payment_method_types on checkout

--- functions/stripe-checkout.js
+++ functions/stripe-checkout.js
@@ -47,13 +47,7 @@ const handler = async event => {
 			allow_promotion_codes: true,
 			success_url: `${SITE_URL}/tickets/status/?status=success&session_id={CHECKOUT_SESSION_ID}`,
 			cancel_url: `${SITE_URL}/tickets/`,
-			payment_method_types: [
-				'card',
-				'ideal',
-				'giropay',
-				'sofort',
-				'sepa_debit'
-			],
+			payment_method_types: ['card', 'giropay', 'sofort', 'sepa_debit'],
 			billing_address_collection: 'required',
 			shipping_rates: shippingRate,
 			shipping_address_collection: {
