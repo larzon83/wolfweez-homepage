15a29904f6efe4fc6cf0d80ab422b440cd68bcbb
Dominic Schwarz <brunooo@gmx.de>
2021-10-20 20:14:23 +0200

    fix(BandDetail): remove 'allowfullscreen' attr from spotify iFrame to silence browser warnings

--- components/BandDetail.vue
+++ components/BandDetail.vue
@@ -84,7 +84,6 @@
 							<iframe
 								:src="`https://open.spotify.com/embed/artist/${band.spotify}`"
 								frameBorder="0"
-								allowfullscreen=""
 								allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
 							></iframe>
 						</v-lazy>
