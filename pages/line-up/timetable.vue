418fcd98b49f321e522908bf7a813a872848e6d7
Dominic Schwarz <brunooo@gmx.de>
2025-04-18 16:21:15 +0200

    feat: allow timetable to be active in dev and preview

--- pages/line-up/timetable.vue
+++ pages/line-up/timetable.vue
@@ -120,7 +120,11 @@ export default {
 
 	data() {
 		return {
-			pageIsActive: false,
+			// pageIsActive: false,
+			pageIsActive:
+				process.env.NODE_ENV === 'development' ||
+				process.env.NUXT_ENV_IS_SPA === 'true' ||
+				process.env.NUXT_ENV_STORYBLOK_PREVIEW === 'true',
 			pageTitle
 		}
 	},
